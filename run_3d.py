e1306211ef9304092bdc7271c4f2365a679fb624
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-01-09 07:19:30 +0000

    Set number of grid levels to 4

--- run_3d.py
+++ run_3d.py
@@ -46,8 +46,8 @@ plot_heterogenous = True
 run_poromechanics = True
 plot_poromechanics = True
 
-# Number of refinement levels for the convergence analysis.
-refinement_levels = 2
+# Number of grid levels for the convergence analysis.
+refinement_levels = 4
 
 
 def run_convergence_analysis(
@@ -239,7 +239,7 @@ def plot_and_save(ax, legend_handles, file_name, y_label):
     ax.grid(which="major", linewidth=1.5)
     ax.grid(which="minor", linewidth=0.75)
     if len(legend_handles) > 0:
-        ax.legend(handles=legend_handles, fontsize=fontsize_legend, loc="upper right")
+        ax.legend(handles=legend_handles, fontsize=fontsize_legend, loc="center left")
 
     border = 0.02
     ax.plot([x_min, x_max], [y_min+border, y_min+border], linestyle="-", color="black", linewidth=1.75)
